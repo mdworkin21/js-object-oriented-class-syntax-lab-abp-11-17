7e9f907ecb4bc48bba05a92f1c6eb37dcb9c776a
Matthew Dworkin <mdworkin21@gmail.com>
2017-11-14 20:10:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,21 @@ class BoardMember{
   this.homeState = homeState
   this.training = training
 
+  veto(){
+    returns `No, I must disagree`}
+
+  + `approve` — returns `You can do that!`
+
+  + `doCharity` — returns `I like to help people.`
+
+  + `releasePressStatement` — returns `You will see great things from Scuber.`
+
+  + `sayHi` — returns `"Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.`
+
+
+
+
+
+
 }
 }
