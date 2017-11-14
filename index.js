da4eb20e39b247e1712bf0325d6d380fe156a4e1
Matthew Dworkin <mdworkin21@gmail.com>
2017-11-14 20:12:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,16 +7,18 @@ class BoardMember{
 
   veto(){
     return `No, I must disagree`}
-/*
-  + `approve` — returns `You can do that!`
 
-  + `doCharity` — returns `I like to help people.`
+  approve(){
+    return `You can do that!`}
+
+  doCharity(){
+     return `I like to help people.`}
 
   + `releasePressStatement` — returns `You will see great things from Scuber.`
 
   + `sayHi` — returns `"Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.`
 
-*/
+
 
 
 
